9451b26993ec8bfe71cded68ad117ecf3cad0e24
Amarnath <118683479+amarnath2482@users.noreply.github.com>
2024-01-11 19:09:36 +0530

    Update Trip.js

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -24,37 +24,31 @@ function Trip() {
         <TripData
           image={weding}
           heading="Wedding"
-          text="It is a profound honor as a photographer to witness two people falling in love."
         />
 
         <TripData
           image={candi}
           heading="Candid"
-          text="Candid photos capture time in its most natural state."
         />
 
         <TripData
           image={brith}
           heading="Birthday"
-          text="From candid smiles to cake-cutting delights, birthday photography freezes the cherished moments of your special day"
         />
 
         <TripData
           image={trade}
           heading="Traditional"
-          text="Timeless and classic, traditional photography captures the essence of your event with an artful touch."
         />
 
         <TripData
           image={cien}
           heading="Cinematic"
-          text="Infusing cinematic flair into every frame, this style transforms your event into a captivating visual narrative."
         />
 
         <TripData
           image={event}
           heading="Event"
-          text="Documenting the heart and soul of gatherings, event photography preserves memories that last a lifetime"
         />
       </div>
       <br></br>
@@ -64,37 +58,31 @@ function Trip() {
           <TripData
             image={makeup}
             heading="Makeup"
-            text="Elevate your beauty with expert makeup services that enhance your natural charm for any occasion"
           />
 
           <TripData
             image={fashion}
             heading="Fashion Style"
-            text="Discover your unique fashion statement with personalized style guidance and trendsetting expertise"
           />
 
           <TripData
             image={cant}
             heading="Catering"
-            text="Savor delectable flavors and impeccable service with our catering, making your event a culinary masterpiece."
           />
 
           <TripData
             image={short}
             heading="Short Films"
-            text="Bring your stories to life in cinematic brilliance through captivating short films that leave a lasting impact."
           />
 
           <TripData
             image={meg}
             heading="Mehndi Design"
-            text="Adorn your hands with intricate and artistic mehndi designs, adding elegance to your special moments."
           />
 
           <TripData
             image={series}
             heading="Web Series"
-            text="Embark on a captivating journey through our compelling web series, offering immersive storytelling and entertainment"
           />
         </div>
       </>
